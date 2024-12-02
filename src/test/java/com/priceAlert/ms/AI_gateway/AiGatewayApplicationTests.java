ebef065e6397bdefbb595cef30e13abde639f939
RenzoRP <renzo.riccipujol@gmail.com>
2024-12-02 10:46:23 +0100

    Initial Spring Boot setup

---
+++ src/test/java/com/priceAlert/ms/AI_gateway/AiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priceAlert.ms.AI_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
